45f68e14a04f737abcd764eb90464b72a83433d8
1 <avhapov@gmail.com>
2022-05-27 00:50:47 +0300

    Весь код

---
+++ tests/api_test.py
@@ -0,0 +1,22 @@
+from run import app
+
+class TestApi:
+
+    def test_app_all_posts_status_code(self):
+        response = app.test_client().get('/api/posts', follow_redirects=True)
+
+        print(response.status_code)
+        print(response.mimetype)
+
+        assert response.status_code == 200, "Статус код запроса всех постов неверный"
+        assert response.mimetype == "application/json", "Получен не JSON"
+
+
+    def test_app_one_posts_status_code(self):
+        response = app.test_client().get('/api/posts/1', follow_redirects=True)
+
+        print(response.status_code)
+        print(response.mimetype)
+
+        assert response.status_code == 200, "Статус код запроса 1 поста неверный"
+        assert response.mimetype == "application/json", "Получен не JSON"
